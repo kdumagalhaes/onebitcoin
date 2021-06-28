dae6efa3e5afd842db9e36c89dd2f634addac5a2
Carlos Pereira <carlospereira@MacBook-Pro-de-Carlos.local>
2021-06-28 20:42:59 -0300

    Finish class 8 part 2

---
+++ src/components/HistoryGraphic/HistoryGraphic.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { View } from "react-native"
+
+//styles
+import styles from "./styles"
+
+const HistoryGraphic = () => {
+    return (
+        <View style={styles.contentGraphic}>
+        </View>
+    )
+}
+
+export default HistoryGraphic
---
+++ src/components/QuotationList/QuotationList.js
@@ -0,0 +1,69 @@
+import React, { Fragment } from 'react'
+import { ScrollView, View, Text, TouchableOpacity } from 'react-native'
+
+//styles
+import styles from './styles'
+
+const QuotationList = () => {
+    return (
+        <Fragment>
+            <View style={styles.filters}>
+                <TouchableOpacity
+                    style={styles.buttonQuery}
+                    onPress={() => { }}
+                >
+                    <Text
+                        style={styles.textButtonQuery}
+                    >
+                        7D
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.buttonQuery}
+                    onPress={() => { }}
+                >
+                    <Text
+                        style={styles.textButtonQuery}
+                    >
+                        15D
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.buttonQuery}
+                    onPress={() => { }}
+                >
+                    <Text
+                        style={styles.textButtonQuery}
+                    >
+                        1M
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.buttonQuery}
+                    onPress={() => { }}
+                >
+                    <Text
+                        style={styles.textButtonQuery}
+                    >
+                        3M
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.buttonQuery}
+                    onPress={() => { }}
+                >
+                    <Text
+                        style={styles.textButtonQuery}
+                    >
+                        6M
+                    </Text>
+                </TouchableOpacity>
+            </View>
+            <ScrollView>
+
+            </ScrollView>
+        </Fragment>
+    )
+}
+
+export default QuotationList
